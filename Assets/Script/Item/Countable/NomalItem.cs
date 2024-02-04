d3bda3b97abdb8b598cd9849ef63899fde78e808
zhaqj12 <zhaqj12@naver.com>
2024-02-04 22:55:25 +0900

    Nomal Item

---
+++ Assets/Script/Item/Countable/NomalItem.cs
@@ -0,0 +1,21 @@
+﻿
+    using UnityEngine;
+
+    [CreateAssetMenu(fileName = "NewEatItem", menuName = "Items/NomalItem")]
+    public class NomalItem : Item
+    {
+        public virtual float GetAmount()
+        {
+            return _amount;
+        }
+
+        public  virtual float GetMaxAmount()
+        {
+            return 0;
+        }
+
+        public virtual void SetAmount(float amount)
+        {
+            _amount = amount;
+        }
+    }

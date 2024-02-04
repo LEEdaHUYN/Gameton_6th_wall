729af5f17ebe7e73b0ce154425f8fc15db9a6d31
zhaqj12 <zhaqj12@naver.com>
2024-02-04 22:39:39 +0900

    RandomItemAddAction.cs

---
+++ Assets/Script/TriggerSystem/Actions/RandomItemAddAction.cs
@@ -0,0 +1,24 @@
+﻿
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class RandomItemAddAction : TriggerAction
+    {
+        public List<ItemInfo> items = new List<ItemInfo>();
+        public override void RunAction()
+        {
+            //50 , 25 , 25
+            float random = Random.Range(0,100);
+            ItemInfo selectItem = new ItemInfo();
+            foreach (var item in items)
+            {
+                if (random <= item.randomValue)
+                {
+                    selectItem = item;
+                    break;
+                }
+            }
+            Managers.Game.AddItem(selectItem.itemName,selectItem.amount);
+           
+        }
+    }

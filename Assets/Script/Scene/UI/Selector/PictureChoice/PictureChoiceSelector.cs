350d477f2cb0e8c50ff6b1022be05332cca95757
zhaqj12 <zhaqj12@naver.com>
2024-02-07 00:13:55 +0900

    PictureSelector

---
+++ Assets/Script/Scene/UI/Selector/PictureChoice/PictureChoiceSelector.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class PictureChoiceSelector : Selector
+{
+    [SerializeField] private TMP_Text _text;
+
+    //TODO : ???
+    public void Init(string text)
+    {
+        
+    }
+    
+    public override void ShowCurrentDay()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void NextDay()
+    {
+        throw new System.NotImplementedException();
+    }
+}

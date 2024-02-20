f502c4a435bec0d095d8125bc3e55b66213a5af3
LEEdaHUYN <150933906+LEEdaHUYN@users.noreply.github.com>
2024-02-20 14:50:25 +0900

    itemdictionarychangeandalive
    아이템사전 완료랑 타이틀씬에 부활ui하다 말았습니다
    마치 찍먹'sㅎ

--- Assets/Script/3D/ItemDictionaryAnimation.cs
+++ Assets/Script/3D/ItemDictionaryAnimation.cs
@@ -1,63 +1,22 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ItemDictionaryAnimation : MonoBehaviour
 {
-    public Image[] Iteminformation;
-    public Image itemspon;
+
     void Start()
     {
-        for (int i = 1; i < Iteminformation.Length; i++)
-        {
-            Iteminformation[i].gameObject.SetActive(true);
-            Iteminformation[i].rectTransform.localScale= new Vector2(1f,1f);
-        }
-        Instantiate(Iteminformation[0], itemspon.rectTransform);
-        ShowImage(Iteminformation[0]);
+
     }
-
-    public void ShowInformation(int currentPage)
-    {
-        //HideIamge(Iteminformation[currentPage - 1]);
-        //Destroy(Iteminformation[currentPage-1]);
-        Iteminformation[currentPage-1].gameObject.SetActive(false);
-        Instantiate(Iteminformation[currentPage],itemspon.rectTransform);
-        Iteminformation[currentPage].gameObject.SetActive(true);
-        //ShowImage(Iteminformation[currentPage]);
-    }
-
-    void HideIamge(Image canvas)
-    {
-        var seq = DOTween.Sequence();
-
-        canvas.transform.localScale = Vector3.one * 0.2f;
-        seq.Append(canvas.transform.DOScale(1.1f, 0.2f));
-        seq.Append(canvas.transform.DOScale(0.2f, 0.2f));
-        seq.Play().OnComplete(() =>
-        {
-            canvas.gameObject.SetActive(false);
-        });
-    }
-
-    void ShowImage(Image canvas)
-    {
-        canvas.gameObject.transform.localScale = Vector3.one * 0.2f;
-        canvas.gameObject.SetActive(true);
-
-        var seq = DOTween.Sequence();
-        seq.Append(canvas.transform.DOScale(1.1f, 0.3f));
-        seq.Append(canvas.transform.DOScale(1f, 0.2f));
-
-        seq.Play();
-    }
-
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
+
+
 }
--- Assets/Script/3D/ScrollviewItem.cs
+++ Assets/Script/3D/ScrollviewItem.cs
@@ -1,10 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
+using System.Xml.Serialization;
 
 public class ScrollviewItem : MonoBehaviour
 {
+    public Sprite[] _Itemiamge;
+
+    public Image line;
+    public Image yellow;
+
+    public Image Itmeimage;
+    public TextMeshProUGUI title;
+    public TextMeshProUGUI undertitle;
+    public TextMeshProUGUI topright;
+    public TextMeshProUGUI bottonright;
+    public TextMeshProUGUI bottonleft;
+
+
+
+    public string[] itmename;
+    public string[] _undertitle;
+    public string[] _topright;
+    public string[] _bottonright;
+    public string[] _bottonleft;
+
+
     [SerializeField]
     private Scrollbar scrollBar;                    // Scrollbar의 위치를 바탕으로 현재 페이지 검사
     private float swipeTime = 0.005f;         // 페이지가 Swipe 되는 시간
@@ -18,10 +43,7 @@ public class ScrollviewItem : MonoBehaviour
     private float valueDistance = 0;            // 각 페이지 사이의 거리
     private int currentPage = 0;            // 현재 페이지
     private int maxPage = 0;                // 최대 페이지
-    private float startTouchX;              // 터치 시작 위치
-    private float endTouchX;                    // 터치 종료 위치
     private bool isSwipeMode = false;       // 현재 Swipe가 되고 있는지 체크
-    private float circleContentScale = 1.6f;    // 현재 페이지의 원 크기(배율)
 
     private void Awake()
     {
@@ -45,12 +67,55 @@ public class ScrollviewItem : MonoBehaviour
     {
         // 최초 시작할 때 0번 페이지를 볼 수 있도록 설정
         SetScrollBarValue(0);
+
+        //currentpage가 바뀌면 작동
+        this.ObserveEveryValueChanged(_ => currentPage)
+            .Subscribe(_ =>
+            {
+                Itmeimage.sprite = _Itemiamge[currentPage];
+                title.text = itmename[currentPage];
+                undertitle.text = _undertitle[currentPage];
+                topright.text = _topright[currentPage];
+                bottonright.text = _bottonright[currentPage];
+                bottonleft.text = _bottonleft[currentPage];
+                TMPDOText(title, 0.5f); TMPDOText(undertitle, 0.8f); TMPDOText(topright, 0.8f);
+                TMPDOText(bottonright, 0.8f); TMPDOText(bottonleft, 0.8f);
+                ShowImage(yellow);
+
+                line.transform.localScale = Vector3.one * 0.2f;
+                var seq = DOTween.Sequence();
+                seq.Append(line.transform.DORotate(new Vector3(0, 0, 100f), 1f));  
+                seq.Join(line.transform.DOScale(1.2f, 0.5f));
+                seq.Join(line.transform.DOScale(1f, 0.5f));
+                seq.Append(line.transform.DORotate(new Vector3(0, 0, 0f), 0.7f));
+             
+
+            });
     }
+
+    public static void TMPDOText(TextMeshProUGUI text,float duration)
+    {
+        text.maxVisibleCharacters = 0;
+        DOTween.To(x => text.maxVisibleCharacters = (int)x, 0f, text.text.Length, duration);
+    }
+
 
     public void SetScrollBarValue(int index)
     {
         currentPage = index;
         scrollBar.value = scrollPageValues[index];
+    }
+
+    void ShowImage(Image canvas)
+    {
+        canvas.gameObject.transform.localScale = Vector3.one * 0.2f;
+        //canvas.gameObject.SetActive(true);
+
+        var seq = DOTween.Sequence();
+        seq.Append(canvas.transform.DOScale(1.3f, 0.5f));
+        seq.Append(canvas.transform.DOScale(1f, 0.3f));
+
+        seq.Play();
     }
 
     private void Update()
@@ -84,7 +149,7 @@ public class ScrollviewItem : MonoBehaviour
 
         isSwipeMode = true;
 
-        ItemScript.ShowInformation(currentPage);
+        //ItemScript.ShowInformation(currentPage);
         while (percent < 1)
         {
             current += Time.deltaTime;

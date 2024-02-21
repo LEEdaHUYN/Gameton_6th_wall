da30fa6e4d11f6511acda906b199b5185f0b19f8
zhaqj12 <zhaqj12@naver.com>
2024-02-21 22:15:25 +0900

    Reward AdMob Completed

--- Assets/Script/Manager/Core/AdManager.cs
+++ Assets/Script/Manager/Core/AdManager.cs
@@ -15,11 +15,22 @@
         
         
         private RewardedAd _rewardedAd;
+        public void RunRewardedAd(Action successCallback,string adUnitId) {
 
+                _adUnitId = adUnitId;
+                LoadRewardedAd(() =>
+                {
+                    //핸들러 등록
+                    RegisterEventHandlers(_rewardedAd);
+                    ShowRewardedAd(successCallback);
+                });
+            
+    }
+        
         /// <summary>
-        /// 보상형 광고 띄우기
+        /// 보상형 로드 하기
         /// </summary>
-        public void LoadRewardedAd(Action successCallback ,string unitId = "ca-app-pub-3940256099942544/5224354917")
+        private void LoadRewardedAd(Action successCallback = null)
         {
             // Clean up the old ad before loading a new one.
             if (_rewardedAd != null)
@@ -27,14 +38,14 @@
                 _rewardedAd.Destroy();
                 _rewardedAd = null;
             }
-
+            
             Debug.Log("Loading the rewarded ad.");
 
             // create our request used to load the ad.
             var adRequest = new AdRequest();
 
             // send the request to load the ad.
-            RewardedAd.Load(unitId, adRequest,
+            RewardedAd.Load(_adUnitId, adRequest,
                 (RewardedAd ad, LoadAdError error) =>
                 {
                     // if error is not null, the load request failed.
@@ -53,5 +64,61 @@
                 });
         }
         
+        /// <summary>
+        /// 보상형 광고 띄우기
+        /// </summary>
+        private void ShowRewardedAd(Action succes)
+        {
+            const string rewardMsg =
+                "Rewarded ad rewarded the user. Type: {0}, amount: {1}.";
+
+            if (_rewardedAd != null && _rewardedAd.CanShowAd())
+            {
+                _rewardedAd.Show((Reward reward) =>
+                {
+                    succes?.Invoke();
+                    // TODO: Reward the user.
+                    Debug.Log(String.Format(rewardMsg, reward.Type, reward.Amount));
+                });
+            }
+        }
         
+        private void RegisterEventHandlers(RewardedAd ad)
+        {
+            // Raised when the ad is estimated to have earned money.
+            ad.OnAdPaid += (AdValue adValue) =>
+            {
+                Debug.Log(String.Format("Rewarded ad paid {0} {1}.",
+                    adValue.Value,
+                    adValue.CurrencyCode));
+                LoadRewardedAd();
+            };
+            // Raised when an impression is recorded for an ad.
+            ad.OnAdImpressionRecorded += () =>
+            {
+                Debug.Log("Rewarded ad recorded an impression.");
+            };
+            // Raised when a click is recorded for an ad.
+            ad.OnAdClicked += () =>
+            {
+                Debug.Log("Rewarded ad was clicked.");
+            };
+            // Raised when an ad opened full screen content.
+            ad.OnAdFullScreenContentOpened += () =>
+            {
+                Debug.Log("Rewarded ad full screen content opened.");
+            };
+            // Raised when the ad closed full screen content.
+            ad.OnAdFullScreenContentClosed += () =>
+            {
+                Debug.Log("Rewarded ad full screen content closed.");
+                LoadRewardedAd();
+            };
+            // Raised when the ad failed to open full screen content.
+            ad.OnAdFullScreenContentFailed += (AdError error) =>
+            {
+                Debug.LogError("Rewarded ad failed to open full screen content " +
+                               "with error : " + error);
+            };
+        }
     }
